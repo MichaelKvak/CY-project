839de63c9e4b6318807738b30e83916f789b6b41
Michael Kvak <mkvak1987@gmail.com>
2024-12-20 20:28:24 +0200

    add new checking

--- cypress/e2e/sample_test.cy.js
+++ cypress/e2e/sample_test.cy.js
@@ -2,5 +2,12 @@ describe("Sample Test", () => {
   it("should visit the example page", () => {
     cy.visit("https://example.cypress.io");
     cy.contains("Kitchen Sink").should("be.visible");
+    cy.get("h2").contains("Commands").should("be.visible");
+    cy.get("p")
+      .invoke("text")
+      .then((text) => {
+        text =
+          'Commands drive your tests in the browser like a real user would. They let you perform actions like typing, clicking, xhr requests, and can also assert things like "my button should be disabled".';
+      });
   });
 });
